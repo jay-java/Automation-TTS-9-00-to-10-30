3ee04a5e0b12c03bd67379d5eca3d6bb5d6b8b32
jay-java <jayprajapati.tops@gmail.com>
2024-09-05 10:05:23 +0530

    open browser

---
+++ Selenium_Learning/src/test/java/basic/P001_OpenBrowser.java
@@ -0,0 +1,20 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P001_OpenBrowser {
+	public static void main(String[] args) throws InterruptedException {
+		//1.for google chrome
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jay-pc\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("https://www.facebook.com/");
+//		driver.manage().window().maximize();
+//		Thread.sleep(2000);
+//		driver.close();
+		
+		
+		//for edge
+		
+	}
+}
